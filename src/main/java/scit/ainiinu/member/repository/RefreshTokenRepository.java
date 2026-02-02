fe74aa2d254ef77a9d2205d10bdf67e111797b40
NineKoo9 <gunhong951@gmail.com>
2026-02-02 23:48:32 +0900

    RefreshToken.token변수명 tokenHash로 변경

--- src/main/java/scit/ainiinu/member/repository/RefreshTokenRepository.java
+++ src/main/java/scit/ainiinu/member/repository/RefreshTokenRepository.java
@@ -1,6 +1,7 @@
 package scit.ainiinu.member.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import scit.ainiinu.member.entity.Member;
 import scit.ainiinu.member.entity.RefreshToken;
 
 import java.util.Optional;
@@ -8,5 +9,6 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByTokenHash(String tokenHash);
     void deleteByTokenHash(String tokenHash);
+    void deleteByMember(Member member);
     Optional<RefreshToken> findByMemberId(Long memberId);
 }
